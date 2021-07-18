fef48dafe436827836a73b7c7b40c7d70993bd8f
HydroZA <jmslegge0@gmail.com>
2021-07-18 10:21:40 +0200

    Major Refactor. Move client to seperate repo

---
+++ src/main/java/yhames/pro/project/afis/responses/MatchResponse.java
@@ -0,0 +1,69 @@
+package yhames.pro.project.afis.responses;
+
+import yhames.pro.project.afis.matchers.Match;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+public class MatchResponse extends Response {
+    private final Match match;
+
+    public MatchResponse(Match mr) {
+        this.match = mr;
+    }
+
+    /*
+        MatchResponses Take the Following format:
+
+        |-----------|-----------|---------------|
+        1 byte      | 4 bytes   | n bytes
+        isMatch     | msg length| msg body
+        boolean     |           |
+        0x00 = true |           |
+        0x01 = false|           |
+
+        NOTE: If isMatch is false then the response will only be a single byte
+        If there is a match then the matched image from the database is sent to
+        the client
+     */
+    private byte[] serialize() {
+        if (match.isMatch()) {
+            ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+            int imgLen = this.match.getFingerprint().getImg().length;
+            lengthBuffer.putInt(imgLen);
+            byte[] lengthBytes = lengthBuffer.array();
+
+            // isMatch + length + img = size of whole message
+            byte[] serialized = new byte[1 + 4 + imgLen];
+
+            // Add 0x00 indicating a good match
+            serialized[0] = 0x00;
+
+            // Add the length of the image
+            System.arraycopy(lengthBytes, 0, serialized, 1, lengthBytes.length);
+
+            // Add the image
+            System.arraycopy(this.match.getFingerprint().getImg(),
+                    0,
+                    serialized,
+                    5,
+                    imgLen
+            );
+
+            return serialized;
+        }
+        else {
+            return new byte[] {
+                    0x01
+            };
+        }
+    }
+
+    // Returns a boolean indicating success
+    public boolean send(OutputStream out) throws IOException{
+        byte[] matchResponseSerialized = this.serialize();
+        out.write(matchResponseSerialized);
+        return true;
+    }
+}
---
+++ src/test/java/yhames/pro/project/afis/AppTest.java
@@ -0,0 +1,163 @@
+package yhames.pro.project.afis;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.lang.Thread;
+
+import yhames.pro.project.afis.matchers.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    private Fingerprint fp;
+    private static final String testDir = "src/test/java/yhames/pro/project/afis/";
+    private static Thread server;
+
+    public AppTest() {
+        try {
+            Path fileName = Path.of("resources/fingerprints/LeftIndex.bmp");
+            fp = new Fingerprint(Files.readAllBytes(fileName));
+        }
+        catch (IOException e) {
+            System.err.println("Unable to find the reference image");
+            fail();
+        }
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        // Start server thread on 6969
+        server = new Thread(new MatchServer());
+        server.start();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        try {
+            MatchServer.stop();
+        }
+        catch (IOException e) {
+            System.err.println("Error while stopping server");
+        }
+    }
+
+    @Test
+    public void testDatabaseDownload() {
+        DatabaseConnection db = new DatabaseConnection();
+
+        byte[] expected = fp.getImg();
+        byte[] actual = db.getFingerprints()[0].getImg();
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    /*
+        Tests searching the database for a match
+    */
+    public void testDatabaseSearch() {
+       Match match = new SourceAFIS().search(fp, new Fingerprint[] {fp});
+
+       System.out.println("Match Score: " + match.getScore());
+
+       Assert.assertTrue(match.isMatch());
+    }
+
+    @Test
+    /*
+        Tests the enrolling of new fingerprints
+    */
+    public void testEnroll() {
+        boolean result = new DatabaseConnection().enroll(fp);
+        Assert.assertTrue(result);
+    }
+    /*
+        Test is 2 images which should NOT be a match
+        are detected as such
+    */
+    @Test
+    public void testMatching() {
+        try {
+            // get a fingerprint from a different finger
+            Fingerprint test = new Fingerprint(
+                Files.readAllBytes(Path.of("resources/fingerprints/LeftHand/LeftMiddle.bmp"))
+            );
+            Match match = new SourceAFIS().match(test, fp);
+
+            System.out.println("Match Score: " + match.getScore());
+
+            Assert.assertFalse(match.isMatch());
+        }
+        catch (IOException e) {
+            fail();
+        }
+    }
+
+    @Test
+    /*
+        Tests the MatchServer by calling a python script which simulates a client
+    */
+    public void testServer() {
+        final int iterations = 2;
+
+        int exitCode = runPythonScript(
+            testDir + "MatchServerTester.py",
+            iterations
+        );
+
+        Assert.assertEquals(0, exitCode);
+    }
+
+    @Test
+    public void testHandshake() {
+        int exitCode = runPythonScript(
+                testDir + "TestHandshake.py",
+                5
+        );
+
+        Assert.assertEquals(0, exitCode);
+
+    }
+
+    @Test
+    /*
+        Tests the Authentication by calling a python script which simulates a client
+    */
+    public void testAuthentication() {
+        final int iterations = 2;
+
+        int exitCode = runPythonScript(
+                testDir + "TestAuthentication.py",
+                iterations
+        );
+
+        Assert.assertEquals(0, exitCode);
+    }
+
+
+    // Returns exit code of python program
+    private int runPythonScript(String path, int iterations) {
+        // Call python script
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "python3", path, String.valueOf(iterations)
+        ).inheritIO();
+
+        try {
+            return processBuilder
+                .start()
+                .waitFor();
+        }
+        catch (Exception e) {
+            return 1; 
+        }
+    }
+}
